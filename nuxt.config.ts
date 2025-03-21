62af6ca1c8bdc6a23506fee7245319d75fb34d81
Saksham Khurana <dawnimpulse@gmail.com>
2025-03-21 19:37:49 +0530

    refactor(config): prettified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-27',
-  future: {
-    compatibilityVersion: 4
-  },
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/icon'],
-  css: ['~/assets/css/main.css'],
-  devServer: {
-    port: 3131
-  }
-})
+    compatibilityDate: "2024-11-27",
+    future: {
+        compatibilityVersion: 4,
+    },
+    devtools: { enabled: true },
+    modules: ["@nuxt/ui", "@nuxt/icon"],
+    css: ["~/assets/css/main.css"],
+    devServer: {
+        port: 3131,
+    },
+});
