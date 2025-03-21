1425dece30aeafe127eb1a5e23778f56d03a26ac
Saksham Khurana <dawnimpulse@gmail.com>
2025-03-21 19:46:05 +0530

    feat(components): website-hero - scrolling text

--- app/components/website/WebsiteHero.vue
+++ app/components/website/WebsiteHero.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const items = ["is lost", "got no battery", "got stolen", "gets locked out"];
+const visible = ref(0);
+
+// Timeout function
+const timeout = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Run some loop in async function
+(async () => {
+    // Loop for 4 times
+    for (let i = 0; i <= 50000; i++) {
+        // Do some stuff
+        visible.value = (visible.value + 1) % 4;
+        // Wait for timeout 1000 ms
+        await timeout(2000);
+    }
+})();
+</script>
 
 <template>
-    <div class="w-full h-full flex flex-col">
+    <div class="w-full h-full flex flex-col justify-between">
         <!--toolbar-->
         <div class="w-full mt-4 flex flex-row items-center">
             <!--title-->
@@ -27,10 +44,22 @@
         </div>
 
         <!--text-->
-        <div class="grow" />
-        <div>A simple way to access your contacts</div>
-        <div>when you are away</div>
-        <div class="grow" />
+        <div class="pt-16 pl-32 flex flex-col text-4xl">
+            <div>A simple way to access your contacts</div>
+            <div class="flex flex-row">
+                <div>when your phone</div>
+                <div v-for="(item, key) in items" class="ml-1">
+                    <div
+                        v-if="key === visible"
+                        class="font-bold tracking-wider motion-preset-slide-up-lg text-green-700"
+                    >
+                        {{ item }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div></div>
     </div>
 </template>
 
