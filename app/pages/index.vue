8d670058ecf94bb21893cba08ed223012f122b2d
Saksham Khurana <dawnimpulse@gmail.com>
2025-03-21 18:11:37 +0530

    feat: init

---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col items-center justify-center gap-4 h-screen">
+    <h1 class="font-bold text-2xl text-(--ui-primary)">
+      Nuxt UI v3
+    </h1>
+
+    <div class="flex items-center gap-2">
+      <UButton
+        label="GitHub"
+        color="neutral"
+        variant="outline"
+        icon="mdi:account-circle"
+        to="https://github.com/nuxt/ui"
+        target="_blank"
+      />
+    </div>
+  </div>
+</template>
