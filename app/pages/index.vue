a0ee9b1ea316ae11dbc377dcb24bca8cc2712246
Saksham Khurana <dawnimpulse@gmail.com>
2025-03-22 17:17:58 +0530

    feat(pages): index - bg animated

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,9 +3,13 @@ import WebsiteHero from "~/components/website/WebsiteHero.vue";
 </script>
 
 <template>
-    <div class="h-screen w-screen flex justify-center">
-        <div class="w-full lg:w-10/12 2xl:w-8/12">
-            <WebsiteHero />
+    <div>
+        <BgAnimatedCircles class="absolute w-screen h-screen" />
+
+        <div class="absolute h-screen w-screen flex justify-center">
+            <div class="w-full lg:w-10/12 2xl:w-8/12">
+                <WebsiteHero />
+            </div>
         </div>
     </div>
 </template>
