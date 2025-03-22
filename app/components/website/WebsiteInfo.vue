21c4dac88096a696a2958284d48f235452c88e10
Saksham Khurana <dawnimpulse@gmail.com>
2025-03-22 18:45:06 +0530

    feat(components): WebsiteInfo

---
+++ app/components/website/WebsiteInfo.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+const data = [
+    {
+        title: "Web App",
+        description:
+            "You can simply use it on your favourite browser without installing :)",
+        icon: "ph:hand-peace-duotone",
+        color: {
+            icon: "text-yellow-500",
+            border: "border-yellow-500",
+            title: "text-yellow-600",
+        },
+    },
+    {
+        title: "Free :)",
+        description: "This app is completely free with no paid features",
+        icon: "ph:hand-peace-duotone",
+        color: {
+            icon: "text-green-500",
+            border: "border-green-500",
+            title: "text-green-600",
+        },
+    },
+    {
+        title: "Multi Device",
+        description:
+            "Use this app on mobile, tablet or desktop. It will look amazing",
+        icon: "material-symbols:devices-outline",
+        color: {
+            icon: "text-blue-500",
+            border: "border-blue-500",
+            title: "text-blue-600",
+        },
+    },
+    {
+        title: "Open Source",
+        description: "The complete code of the product is available on github",
+        icon: "mdi-github",
+        color: {
+            icon: "text-black-500",
+            border: "border-black-500",
+            title: "text-black-600",
+        },
+    },
+];
+</script>
+
+<template>
+    <div class="h-screen">
+        <div class="flex flex-col items-center">
+            <!--title-->
+            <div
+                class="text-8xl underline underline-offset-8 decoration-dotted text-black"
+            >
+                Features
+            </div>
+
+            <!--grid-->
+            <div
+                class="w-full mt-48 gap-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
+            >
+                <div
+                    :class="`bg-white h-48 px-4 py-4 rounded-2xl border-4 ${item.color.border}`"
+                    v-for="item in data"
+                >
+                    <div class="flex flex-col">
+                        <div class="flex flex-row items-center">
+                            <Icon
+                                :name="item.icon"
+                                size="36"
+                                :class="`${item.color.icon}`"
+                            />
+                            <div
+                                :class="`ml-4 text-2xl font-bold ${item.color.title}`"
+                            >
+                                {{ item.title }}
+                            </div>
+                        </div>
+                        <div class="mt-4">{{ item.description }}</div>
+                    </div>
+                </div>
+            </div>
+
+            <!--app-->
+            <UButton
+                class="mt-48 px-16 py-4 rounded-xl animate-bounce"
+                color="neutral"
+            >
+                Go to App
+            </UButton>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
