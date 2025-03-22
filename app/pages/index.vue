1555b8054502e018d1291b7e200994077ce3ed5d
Saksham Khurana <dawnimpulse@gmail.com>
2025-03-22 18:45:49 +0530

    feat(pages): index - websiteInfo

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import WebsiteHero from "~/components/website/WebsiteHero.vue";
+import WebsiteInfo from "~/components/website/WebsiteInfo.vue";
 </script>
 
 <template>
     <div>
         <BgAnimatedCircles class="absolute w-screen h-screen" />
 
-        <div class="absolute h-screen w-screen flex justify-center">
-            <div class="w-full lg:w-10/12 2xl:w-8/12">
-                <WebsiteHero />
+        <div class="absolute w-screen flex justify-center">
+            <div class="flex flex-col w-full lg:w-10/12 2xl:w-8/12">
+                <WebsiteHero class="h-screen" />
+                <WebsiteInfo />
             </div>
         </div>
     </div>
