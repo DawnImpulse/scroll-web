5b7f2ab6fc41f79a625ce7e63a620ff7f4154283
Saksham Khurana <dawnimpulse@gmail.com>
2025-03-26 18:40:49 +0530

    feat(app): auth check

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,17 @@
 <template>
-  <UApp>
-    <NuxtPage />
-  </UApp>
+    <UApp>
+        <NuxtPage />
+    </UApp>
 </template>
+
+<script setup lang="ts">
+import PocketBase from "pocketbase";
+
+const route = useRoute();
+const config = useRuntimeConfig();
+const pb = new PocketBase(config.public.backend);
+
+if (!route.path.includes("/app/auth") && route.path.includes("/app")) {
+    if (!pb.authStore.isValid) navigateTo("/app/auth/login");
+}
+</script>
