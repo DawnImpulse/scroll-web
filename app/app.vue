b8985cacc8b41484958167e68ca8bc4b06cd556a
Saksham Khurana <dawnimpulse@gmail.com>
2025-03-27 17:10:38 +0530

    feat(app): using toast config & auth check using firebase

--- app/app.vue
+++ app/app.vue
@@ -1,17 +1,18 @@
 <template>
-    <UApp>
+    <UApp :toaster="toaster">
         <NuxtPage />
     </UApp>
 </template>
 
 <script setup lang="ts">
-import PocketBase from "pocketbase";
-
 const route = useRoute();
-const config = useRuntimeConfig();
-const pb = new PocketBase(config.public.backend);
+const toaster = {
+    position: "top-center",
+};
 
+// wait for fire auth to be ready
+const user = await getCurrentUser();
 if (!route.path.includes("/app/auth") && route.path.includes("/app")) {
-    if (!pb.authStore.isValid) navigateTo("/app/auth/login");
+    if (!user) navigateTo("/app/auth/login");
 }
 </script>
