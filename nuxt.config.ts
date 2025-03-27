152972db7fb9cc1c32c38401ce0924ee840c811f
Saksham Khurana <dawnimpulse@gmail.com>
2025-03-27 17:10:15 +0530

    feat(config): vuefire

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: false,
     compatibilityDate: "2024-11-27",
     future: {
         compatibilityVersion: 4,
     },
     devtools: { enabled: true },
-    modules: ["@nuxt/ui-pro", "@nuxt/icon"],
+    modules: ["@nuxt/ui-pro", "@nuxt/icon", "nuxt-vuefire"],
     css: ["~/assets/css/main.css"],
     devServer: {
         port: 3131,
@@ -13,6 +14,21 @@ export default defineNuxtConfig({
     runtimeConfig: {
         public: {
             backend: process.env.PB_HOST,
+        },
+    },
+    vuefire: {
+        auth: {
+            enabled: true,
+        },
+        config: {
+            apiKey: "AIzaSyBzRB8qX_QAI0D2DeCiPMH0E5UB5G6A_fg",
+            authDomain: "scroll-contacts.firebaseapp.com",
+            projectId: "scroll-contacts",
+            storageBucket: "scroll-contacts.firebasestorage.app",
+            messagingSenderId: "908465068358",
+            appId: "1:908465068358:web:049871a2c6081f4af2ed47",
+            databaseURL:
+                "https://scroll-contacts-default-rtdb.europe-west1.firebasedatabase.app/",
         },
     },
 });
