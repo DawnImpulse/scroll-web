e12141cd4ce1445a4a8c1641f125a859383029b0
Saksham Khurana <dawnimpulse@gmail.com>
2025-03-28 18:06:25 +0530

    feat(components): authVerify

---
+++ app/components/auth/AuthVerify.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+    <!--title-->
+    <div class="text-xl tracking-wider text-black">Verification</div>
+
+    <!--description-->
+    <div class="text-xs tracking-wider text-gray-400">
+        check your inbox for <br />
+        verification email & confirm it
+    </div>
+
+    <UIcon
+        name="mdi:loading"
+        class="mt-8 animate-spin self-center text-(--ui-primary)"
+        size="48"
+    />
+
+    <!--logo-->
+    <div class="mt-8 flex items-center self-center">
+        <img src="@/assets/icons/logo.svg" class="size-6" alt="" />
+        <div class="ml-2 text-sm text-neutral-500">
+            <b class="text-(--ui-primary)">Scroll</b> by Codevry
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
