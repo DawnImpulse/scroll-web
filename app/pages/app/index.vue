0dfcba7ba41095d5311b619eeaece9b08a7a8ef3
Saksham Khurana <dawnimpulse@gmail.com>
2025-04-02 17:12:55 +0530

    feat(pages): app - added viewDialogAdd

--- app/pages/app/index.vue
+++ app/pages/app/index.vue
@@ -3,6 +3,7 @@ import AppBar from "~/components/navigation/AppBar.vue";
 import { useDatabaseList } from "vuefire";
 import { getDatabase, ref as dbRef } from "firebase/database";
 import ViewListContacts from "~/components/views/ViewListContacts.vue";
+import ViewDialogAdd from "~/components/views/ViewDialogAdd.vue";
 
 const user = await getCurrentUser();
 const database = getDatabase();
@@ -12,15 +13,12 @@ const { pending, data: items } = useDatabaseList(itemsRef);
 
 <template>
     <div class="flex w-screen h-screen justify-center bg-zinc-50">
-        <div class="flex flex-col xl:w-8/12">
+        <div class="flex flex-col lg:w-10/12 xl:w-8/12">
             <!--appbar-->
             <AppBar />
 
-            <UButton
-                icon="material-symbols:add-circle"
-                class="mt-16 w-fit self-end px-4 py-2"
-                label="Contact"
-            />
+            <!--add new-->
+            <ViewDialogAdd v-if="!pending" class="mt-16 w-fit self-end" />
 
             <!--loading/empty-->
             <LayoutEmptyScreen
