6a778d2a6186db39693db7d1afa4eb5ae3751909
Saksham Khurana <dawnimpulse@gmail.com>
2025-04-02 17:18:49 +0530

    fix(view): grid responsive

--- app/components/views/ViewListContacts.vue
+++ app/components/views/ViewListContacts.vue
@@ -6,8 +6,10 @@ defineProps(["items"]);
 
 <template>
     <div>
-        <div class="grid grid-cols-3" v-for="item in items" :key="item.id">
-            <UIContact :item="item" />
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+            <div v-for="item in items" :key="item.id">
+                <UIContact :item="item" />
+            </div>
         </div>
     </div>
 </template>
