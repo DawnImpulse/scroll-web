b18f198e77df0bb13702ea0bedf36b86d4c3da52
Saksham Khurana <dawnimpulse@gmail.com>
2025-04-02 12:42:50 +0530

    feat(container): btn github

---
+++ app/components/container/BtnGithub.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+    <UButton class="mr-8" icon="mdi:github" variant="outline" color="neutral">
+        Github
+    </UButton>
+</template>
+
+<style scoped></style>
