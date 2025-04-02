91491f9e1ece2d5b5e45f02aa71436f1f8c03c3d
Saksham Khurana <dawnimpulse@gmail.com>
2025-04-02 18:48:02 +0530

    feat(components): btn - github open link

--- app/components/container/BtnGithub.vue
+++ app/components/container/BtnGithub.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function github() {
+    open("https://github.com/DawnImpulse/scroll-web");
+}
+</script>
 
 <template>
-    <UButton class="mr-8" icon="mdi:github" variant="outline" color="neutral">
+    <UButton
+        class="mr-8"
+        icon="mdi:github"
+        variant="outline"
+        color="neutral"
+        @click="github"
+    >
         Github
     </UButton>
 </template>
