b5d05c0bdd7790c03fa07d2fd9a7b58cfb97da7a
Saksham Khurana <dawnimpulse@gmail.com>
2025-04-02 12:43:09 +0530

    feat(views): listContacts

---
+++ app/components/views/ViewListContacts.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import UIContact from "~/components/ui/UIContact.vue";
+
+defineProps(["items"]);
+</script>
+
+<template>
+    <div>
+        <div class="grid grid-cols-3" v-for="item in items" :key="item.id">
+            <UIContact :item="item" />
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
