52eb2a7cd60f9bbfd1a0df5a84e15e4e5a6b71a0
Saksham Khurana <dawnimpulse@gmail.com>
2025-04-02 12:42:20 +0530

    feat(ui): contact - change path + adding buttons

---
+++ app/components/ui/UIContact.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps(["item"]);
+</script>
+
+<template>
+    <div class="flex flex-col px-6 py-4 rounded-lg shadow-2xl bg-white">
+        <!--name-->
+        <div>{{ item.name }}</div>
+
+        <!--number & actions-->
+        <div class="flex flex-row space-between">
+            <!--number-->
+            <div class="mt-1 grow text-black text-lg font-bold tracking-wider">
+                {{ item.phone }}
+            </div>
+
+            <!--actions-->
+            <div class="flex items-center">
+                <UButton
+                    color="error"
+                    variant="ghost"
+                    icon="material-symbols:delete"
+                    size="md"
+                />
+                <UButton
+                    color="neutral"
+                    variant="ghost"
+                    class="ml-4"
+                    icon="material-symbols:edit"
+                    size="md"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
