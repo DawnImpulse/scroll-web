a9ad6d4a39b3ae81e4fb91a61eb227d6016cc763
Saksham Khurana <dawnimpulse@gmail.com>
2025-04-05 15:00:58 +0530

    feat(config): replaced @nuxt/ui-pro with ui only

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
         compatibilityVersion: 4,
     },
     devtools: { enabled: true },
-    modules: ["@nuxt/ui-pro", "@nuxt/icon", "nuxt-vuefire"],
+    modules: ["@nuxt/ui", "@nuxt/icon", "nuxt-vuefire"],
     css: ["~/assets/css/main.css"],
     devServer: {
         port: 3131,
