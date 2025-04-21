9e862fad11ac33da6362043b3f03c7894a97a35f
Saksham <dawnimpulse@gmail.com>
2025-04-21 14:21:11 +0530

    feat: seo handling

--- app/app.vue
+++ app/app.vue
@@ -5,6 +5,17 @@
 </template>
 
 <script setup lang="ts">
+useSeoMeta({
+    title: "Scroll",
+    ogTitle: "Scroll",
+    description:
+        "A simple way to access your contacts when your phone gets lost/stolen/out-of-battery",
+    ogDescription:
+        "A simple way to access your contacts when your phone gets lost/stolen/out-of-battery",
+    ogImage: "https://scroll.codevry.com/meta.png",
+    twitterCard: "summary_large_image",
+});
+
 const route = useRoute();
 const toaster = {
     position: "top-center",
